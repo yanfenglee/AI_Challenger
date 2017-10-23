079df13b7e42f24f9b9b3d1afdcd8da2617cd1e7
liyanfeng <yanfeng.li@medishare.cn>
2017-10-23 18:34:33 +0800

    aa

--- Baselines/caption_baseline/im2txt/train.sh
+++ Baselines/caption_baseline/im2txt/train.sh
@@ -5,7 +5,7 @@ TFRECORD_DIR="$ML_DATA/challenge/train/output"
 INCEPTION_CHECKPOINT="$ML_DATA/challenge/inception_v3.ckpt"
 MODEL_DIR="$ML_DATA/challenge/train/output/model"
 #export CUDA_VISIBLE_DEVICES="1"
-python train.py \
+python im2txt/train.py \
   --input_file_pattern="${TFRECORD_DIR}/train-?????-of-00160" \
   --inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
   --train_dir="${MODEL_DIR}/train" \
