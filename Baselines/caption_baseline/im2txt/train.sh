b114b00be0cd1db5cf8c555bb0380913ecec9403
liyanfeng <yanfeng.li@medishare.cn>
2017-10-23 21:44:16 +0800

    aa

--- Baselines/caption_baseline/im2txt/train.sh
+++ Baselines/caption_baseline/im2txt/train.sh
@@ -9,4 +9,4 @@ python im2txt/train.py \
   --input_file_pattern="${TFRECORD_DIR}/train-?????-of-00160" \
   --inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
   --train_dir="${MODEL_DIR}/train" \
-  --number_of_steps=500 #> ${outpath} 2>&1 &
+  --number_of_steps=1000000 #> ${outpath} 2>&1 &
