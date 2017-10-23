45ff3ed37c0aa384d06802750500760d7beef8f8
liyanfeng <yanfeng.li@medishare.cn>
2017-10-23 15:24:50 +0800

    aa

--- Baselines/caption_baseline/build_tfrecord.sh
+++ Baselines/caption_baseline/build_tfrecord.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES="1"
-python build_tfrecord.py --image_dir=$ML_DATA/challenge/train/images\
+python build_tfrecord.py --image_dir=$ML_DATA/challenge/train/images_org\
                         --captions_file=$ML_DATA/challenge/train/caption.json\
                         --output_dir=$ML_DATA/challenge/train_build\
                         --train_shards=160\
