27c5020b8f534cf9952d42c8f1c2c6ba542a92cf
liyanfeng <yanfeng.li@medishare.cn>
2017-10-23 20:56:36 +0800

    aa

--- Baselines/caption_baseline/im2txt/inference.sh
+++ Baselines/caption_baseline/im2txt/inference.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-cd /your_work_dir
-CHECKPOINT_PATH="/your_checkpoint_dir"
-VOCAB_FILE="/your_word_list_dir/word_counts.txt"
+CHECKPOINT_PATH="$ML_DATA/challenge/train/output/model/train/model.ckpt-500"
+VOCAB_FILE="$ML_DATA/challenge/train/output/word_counts.txt"
 export CUDA_VISIBLE_DEVICES="0"
-IMAGE_DIR='/your_image_dir/caption_test1_images_20170923/'
-OUTJSON='/your_output_dir/your_output.json'
+IMAGE_DIR="$ML_DATA/challenge/val/images_org"
+OUTJSON="$ML_DATA/challenge/val/out.json"
 python run_inference.py \
  	--checkpoint_path=${CHECKPOINT_PATH} \
   	--vocab_file=${VOCAB_FILE} \
